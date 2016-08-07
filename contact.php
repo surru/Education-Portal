fbf972897f60e5ddf3965c9683b0f9967c447cea
surru <saurabhkhoria7@gmail.com>
2016-08-08 00:17:16 +0530

    MAINT: Add contact.php

---
+++ contact.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+include('header.php')
+?>
+
+<h1><b><font size="6">CONTACT US</font></b></h1>
+<h1><font size = "6"><i>Saurabh Khoria</i> (Designer & Developer)</font></h1>
+<p><br>
+<img src="Images/sa.jpg" style="width:240px;height:228px"><br>
+
+<font size="3">
+B.Tech 3rd Year, Computer Science & Engineering<br>
+Indian Institute of Technology Ropar<br>
+Email :- saurabh.khoria@iitrpr.ac.in<br>
+Mobile :- +919530973030<br>
+
+</font>
+
+<a href="https://www.facebook.com/saurabh.khoria.7">
+<img src="Images/fb.png" style="width:35px;height:35px"></a>
+
+
+<a href="https://in.linkedin.com/in/saurabh-khoria-0796aa85">
+<img src="Images/in.png" style="width:35px;height:35px"></a>
+
+
+<a href="https://angel.co/saurabh-khoria-7">
+<img src="Images/ang.png" style="width:35px;height:35px"></a>
+
+<a href="mailto:saurabh.khoria@iitrpr.ac.in">
+<img src="Images/gm.jpg" style="width:35px;height:35px"></a>
+
+</p>
+
+<?php
+    include('footer.php')
+?>
