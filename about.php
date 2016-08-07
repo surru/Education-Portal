66ed53b1797ab84a1e7acdcb80256b090f71ad9f
surru <saurabhkhoria7@gmail.com>
2016-08-07 19:38:58 +0530

    MAINT: Add about.php

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+include('header.php')
+?>
+
+
+    <p><font size="6">ABOUT US</font></p><br><br><br>
+<div class="center">
+This website is an academic project submitted as an assignment of the course Software Systems Laboratory. This website caters to the need of students who wish to find educational resources on the internet like books and lectures.Although, we have added only videos tutorials of C, Java, Python, GIMP, PHP, Introduction to Algorithms. We have included options like signing in only for some restricted users ,searching the videos, commenting on the videos for some restricted users, and option of like. Very soon we will come up with a question and answer forum also.
+
+
+<h3>HELP </h3>
+
+Tips for a first time user:
+<br>
+1. For downloading the e-books, you can follow the link Home->Books.
+<br>	Choose any one of the books and it will open in your browser. You can download this book.
+	<br>
+2. One can see the videos by going to the link Home->Videos.
+<br>3. We provide you with an option to search the videos at this link. The list will be provided by clicking on the Go button.
+<br>4. Choose the desired video with the search result. The video will open in the window.
+<br>5. However, to mention any comment, you will have to sign in. Even clicking on post button will redirect to the login page. After logging in, you will be able to post comments.
+
+</div>
+
+<?php
+    include('footer.php')
+?>
+
