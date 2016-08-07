c4eb178ef5590e2da9b621e0ae3c95eaa124f258
surru <saurabhkhoria7@gmail.com>
2016-08-07 18:50:51 +0530

    MAINT: Add footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+
+</div>
+  </div>
+</div>
+<div align = "center"id = "header">
+Website design for IIT usage only <a href="http://http://www.iitrpr.ac.in/"> </a>Best hosted at <a href="http://www.iitrpr.ac.in" target="_blank">www.iitrpr.ac.in</a>
+</div>
+
+</body>
+</html>
