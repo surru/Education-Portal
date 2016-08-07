9832c6ab1d9f6b1f47b2c5d2a418efbe11389822
surru <saurabhkhoria7@gmail.com>
2016-08-08 00:21:53 +0530

    MAINT: Add search_bar.php

---
+++ search_bar.php
@@ -0,0 +1,12 @@
+<!-- Search Bar -->
+
+<center>
+<form action="searching.php" method="post">
+    Search Videos :
+    <input type="text" name="search">
+
+    <input type="submit" value="GO">
+</form>
+<br>
+<br>
+</center>
