ae710a714960d15ef5b6e34118dd11c456401ee0
surru <saurabhkhoria7@gmail.com>
2016-08-07 19:36:16 +0530

    MAINT: Add books.php

---
+++ books.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+include('header.php')
+?>
+
+    <h1><font size="6" >BOOKS</font></h1>
+<br><br>
+<ul id="button">
+	<li><a href="Books/K&R.pdf">Dennis Ritchie - Programming Language Ansi C</a></li><br>
+	<li><a href="Books/Cormen.pdf">Introduction to Algorithms- CLRS</a></li><br>
+
+	<li><a href="Books/66_Concepts of Programming Languages 10th-Sebesta.PDF">Concepts of Programming Languages, Robert Sebesta</a></li><br>
+	<li><a href="Books/bartle_introduction-to-real-analysis-new-edition.pdf">Real Analysis, Bartle and Sherbet</a></li><br>
+	<li><a href="Books/p.pdf">A First Course in Probability, Sheldon Ross</a></li><br>
+	<li><a href="Books/james_ward_brown_ruel_vance_churchill_complex_vbookfi-org.pdf">Complex Variables and their Applications, Brown & Churchill</a></li><br>
+	<li><a href="Books/patterson_book.pdf">Computer Architecture, Hennesy</a></li><br>
+	<li><a href="Books/Stallings.pdf">Computer Architecture, William Stallings</a></li><br>
+	<li><a href="Books/Signals.pdf">Signals and Systems, BP Lathi</a></li><br>
+	<li><a href="Books/Mano.pdf">Digital Design, M. Morris Mano</a></li><br>
+	<li><a href="Books/java_tutorial.pdf">Java Tutorial</a></li><br>
+	<li><a href="Books/Differential.pdf">Differential Equations, E.A. Codington</a></li><br>
+	<li><a href="Books/Callister.pdf">Callister</a></li><br>
+
+	<li><a href="Books/Nil.pdf">Electric Circuits, Nilson Riedel</a></li><br>
+	<li><a href="Books/Macro.pdf">Macro-Economics</a></li><br>
+	<li><a href="Books/Thomas.pdf">Thomas Calculus</a></li><br>
+	<li><a href="Books/Carstairs.pdf">Introduction to English morphology, Carstairs</a></li><br>
+	<li><a align href="Books/tutorial.pdf">C++ language tutorial	</a></li><br>
+	</ul>
+
+<?php
+    include('footer.php')
+?>
+
