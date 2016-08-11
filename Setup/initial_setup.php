677d60a374a9367c709eb20f4eaadda7ce5cc674
surru <saurabhkhoria7@gmail.com>
2016-08-11 19:03:26 +0530

    MAINT: Add initial_setup.py

---
+++ Setup/initial_setup.php
@@ -0,0 +1,14 @@
+<?php
+
+include('create_database.php');
+include('create_users.php');
+include('create_table_Algo.php');
+include('create_table_C.php');
+include('create_table_Gimp.php');
+include('create_table_Java.php');
+include('create_table_Latex.php');
+include('create_table_PHP.php');
+include('create_table_Python.php');
+include('create_table_comment.php');
+
+?>
